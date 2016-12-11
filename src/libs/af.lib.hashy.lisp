1bf8299ddb4d02b26dd83cfeedd287b0977ee3eb
Matthew Carter <m@ahungry.com>
2016-12-11 02:12:28 -0500

    Working string key correction

--- src/libs/af.lib.hashy.lisp
+++ src/libs/af.lib.hashy.lisp
@@ -34,6 +34,29 @@
 
 (in-package #:af.lib.hashy)
 
+(defun stringify (symbol)
+  "Turn a cl-json converted string from the symbol back
+into the appropriate string.
+
+This primarily changes things like *this to This and one-two to oneTwo.
+
+We will probably have an issue in the future when there is a legitimate
+dash in a key name..."
+  (let ((string (string-downcase (string symbol)))
+        (last))
+    (remove-if
+     (lambda (c) (or (eq #\* c) (eq #\- c))) ;; Remove * chars
+     (map 'string
+          (lambda (c)
+            (let (char)
+              (cond
+                ((eq last #\*) (setf char (char (string-upcase c) 0)))
+                ((eq last #\-) (setf char (char (string-upcase c) 0)))
+                (t (setf char c)))
+              (setf last c)
+              char))
+          string))))
+
 (defun dotp (pair)
   "T if PAIR is a dotted pair and not a list."
   (not (listp (cdr pair))))
@@ -44,7 +67,7 @@
     (cond
       ;; We receive something like '(:this . "that")
       ((dotp alist)
-       (setf (gethash (string-downcase (string (car alist))) hash)
+       (setf (gethash (stringify (car alist)) hash)
              (cdr alist)))
 
       ;; We receive something like '(1 2 3)
@@ -55,25 +78,17 @@
       ((listp (car (car alist)))
        (mapcar (lambda (item)
                  (alist-to-hash item))
-               alist)
-       )
+               alist))
 
       ;; We receive something like '(:here . ((:we . "go")))
       ;; Convert each item in the alist to a hash key
       (t
        (loop for item in alist
-          do (if
-              (listp (car item))
-              (progn
-                (print "WTF")
-                (list (alist-to-hash item))
-                ;;(setf (gethash "??" hash) (alist-to-hash item))
-                )
-              (let ((key (string-downcase (string (car item))))
+          do (let ((key (stringify (car item)))
                    (item (cdr item)))
                (if (listp item) ;; We know it isn't an atom
                    (setf (gethash key hash) (alist-to-hash item))
-                   (setf (gethash key hash) item)))))
+                   (setf (gethash key hash) item))))
        hash))))
 
 (defun alist-from-json-file (filename)
